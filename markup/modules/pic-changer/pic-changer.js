28ea95cba72af2af8f9284b33f9717f1c90d95f2
Sosnovsky Alexey <sosnovsky.as@gmail.com>
2016-01-30 11:57:32 +0300

    upgrade

--- markup/modules/pic-changer/pic-changer.js
+++ markup/modules/pic-changer/pic-changer.js
@@ -1,74 +1,79 @@
-function PicChanger(options) {
-  var elem = options.elem;
+;
+(function () {
+  'use strict';
 
-  var xmlhttp = new XMLHttpRequest();
-  var url = options.imageListFile;
-  var picPath = options.staticPath;
+  function PicChanger(options) {
+    var elem = options.elem;
 
-  // INIT
-  xmlhttp.onreadystatechange = function () {
-    // забираем с сервера список изображений из JSON файла
-    if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
-      var picList = JSON.parse(xmlhttp.responseText);
+    var xmlhttp = new XMLHttpRequest();
+    var url = options.imageListFile;
+    var picPath = options.staticPath;
 
-      // создаём обёртку для изображений
-      var pcList = document.createElement('div');
-      pcList.className = 'pic-changer_pic-list';
-      elem.appendChild(pcList);
+    // INIT
+    xmlhttp.onreadystatechange = function () {
+      // забираем с сервера список изображений из JSON файла
+      if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+        var picList = JSON.parse(xmlhttp.responseText);
 
-      // заполняем img
-      for (i = 0; i < 24; ++i) {
-        // генерим случайное число в рамках массива изображений
-        var randomNum = Math.floor(Math.random() * picList.pics.length);
+        // создаём обёртку для изображений
+        var pcList = document.createElement('div');
+        pcList.className = 'pic-changer_pic-list';
+        elem.appendChild(pcList);
 
-        var pcListItem = document.createElement('img');
-        pcListItem.className = 'pic-changer_pic-item';
+        // заполняем img
+        for (var i = 0; i < 24; ++i) {
+          // генерим случайное число в рамках массива изображений
+          var randomNum = Math.floor(Math.random() * picList.pics.length);
 
-        // заполняем img случайным изображением
-        pcListItem.setAttribute('src', picPath + picList.pics[randomNum]);
+          var pcListItem = document.createElement('img');
+          pcListItem.className = 'pic-changer_pic-item';
 
-        // помещяем изображения в обёртку
-        pcList.appendChild(pcListItem);
+          // заполняем img случайным изображением
+          pcListItem.setAttribute('src', picPath + picList.pics[randomNum]);
+
+          // помещяем изображения в обёртку
+          pcList.appendChild(pcListItem);
+        }
+
+        var imgs = pcList.querySelectorAll('img');
+
+        // запускаем таймер смены случайного изображения
+        var changeTimer = window.setInterval(function () {
+          var randomImg = Math.floor(Math.random() * imgs.length);
+          var randomPic = Math.floor(Math.random() * picList.pics.length);
+          imgs[randomImg].setAttribute('src', picPath + picList.pics[randomPic]);
+        }, 500);
+
+        // создаём обёртку для изображений
+        var pcContent = document.createElement('div');
+        pcContent.className = 'pic-changer_content';
+
+        // создаём заголовок контента
+        var pcContentHeader = document.createElement('div');
+        pcContentHeader.className = 'pic-changer_content-header';
+        pcContentHeader.textContent = options.header;
+        pcContent.appendChild(pcContentHeader);
+
+        // создаём текст контента
+        var pcContentText = document.createElement('div');
+        pcContentText.className = 'pic-changer_content-text';
+        pcContentText.textContent = options.text;
+        pcContent.appendChild(pcContentText);
+
+        elem.appendChild(pcContent);
       }
+    };
 
-      var imgs = pcList.querySelectorAll('img');
+    // отправляем запрос
+    xmlhttp.open('GET', url, true);
+    xmlhttp.send();
+  }
 
-      // запускаем таймер смены случайного изображения
-      var changeTimer = window.setInterval(function () {
-        var randomImg = Math.floor(Math.random() * imgs.length);
-        var randomPic = Math.floor(Math.random() * picList.pics.length);
-        imgs[randomImg].setAttribute('src', picPath + picList.pics[randomPic]);
-      }, 500);
-
-      // создаём обёртку для изображений
-      var pcContent = document.createElement('div');
-      pcContent.className = 'pic-changer_content';
-
-      // создаём заголовок контента
-      var pcContentHeader = document.createElement('div');
-      pcContentHeader.className = 'pic-changer_content-header';
-      pcContentHeader.textContent = options.header;
-      pcContent.appendChild(pcContentHeader);
-
-      // создаём текст контента
-      var pcContentText = document.createElement('div');
-      pcContentText.className = 'pic-changer_content-text';
-      pcContentText.textContent = options.text;
-      pcContent.appendChild(pcContentText);
-
-      elem.appendChild(pcContent);
-    }
-  };
-
-  // отправляем запрос
-  xmlhttp.open('GET', url, true);
-  xmlhttp.send();
-}
-
-var picChanger = new PicChanger({
-  elem: document.querySelector('.pic-changer'),
-  header: 'Заголовок',
-  text: 'Текстовое описание использования данного приложения',
-  staticPath: '/static/img/content/',
-  imageListFile: 'pic-changer.json'
-});
+  var picChanger = new PicChanger({
+    elem: document.querySelector('.pic-changer'),
+    header: 'Заголовок',
+    text: 'Текстовое описание использования данного приложения',
+    staticPath: '/static/img/content/',
+    imageListFile: 'pic-changer.json'
+  });
+}());
